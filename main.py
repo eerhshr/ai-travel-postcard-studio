c6c57aef74e024a48632b9315bdbcfda75965ce1
rshreeharini <mylearningsasigo@gmail.com>
2025-08-29 15:04:10 -0600

    generate images

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ API_KEY = os.getenv("GEN_AI_API_KEY")
 client = genai.Client(api_key=API_KEY)
 print("Google Generative AI client initialized.")
 
-prompt = "Create a vintage postcard using the image uploaded. Add the text 'Greetings' in a retro font."
-
-image = Image.open("utah.jpg")
+image_path = "colorado.jpg"
+image = Image.open(image_path)
 image = image.resize((256, 256))  #resize
+
+location = os.path.splitext(os.path.basename(image_path))[0].capitalize()
+
+
+prompt = f"Create a vintage postcard using the image uploaded. Add the text Greetings from '{location}' text in a retro font."
+print("Prompt:", prompt)
 
 try:
     response = client.models.generate_content(
@@ -28,7 +33,7 @@ try:
             print(part.text)
         elif part.inline_data is not None:
             image = Image.open(BytesIO(part.inline_data.data))
-            image.save("generated_postcard_image.png")
+            image.save(f"{location}_postcard_image.png")
             print("Image saved as generated_postcard_image.png")
     time.sleep(1)
 except Exception as e:
